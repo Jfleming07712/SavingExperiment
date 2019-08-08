d3ea0506ec54da3c6269ae08d58921ca8ab5d3e2
Jfleming <Jfleming07712@gmail.com>
2019-08-08 15:53:56 -0400

    Let the experiments Begin

--- SavingExperiment/Program.cs
+++ SavingExperiment/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
 
 namespace SavingExperiment
 {
@@ -22,10 +25,18 @@ namespace SavingExperiment
 
             // Trying to save the gameContext
 
+            // serialize JSON to a string and then write string to a file
+            File.WriteAllText(@"c:\CelestialTravels\Save\GameSave.json", JsonConvert.SerializeObject(gameContext));
+
+            // Trying to Load the saved gameContext
+
+            // read file into a string and deserialize JSON to a type
+            GameContext gameContextLoad = JsonConvert.DeserializeObject<GameContext>(File.ReadAllText(@"c:\CelestialTravels\Save\GameSave.json"));
 
 
 
-            Console.WriteLine(gameContext.Name + "  " + gameContext.Age + "  " + gameContext.Bag + "  " + gameContext.Bag.Name1 + "  " + gameContext.Bag.Full);
+
+            Console.WriteLine(gameContextLoad.Name + "  " + gameContextLoad.Age + "  " + gameContextLoad.Bag + "  " + gameContextLoad.Bag.Name1 + "  " + gameContextLoad.Bag.Full);
 
         }
     }
